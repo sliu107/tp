623ff2d289d7247ab486616cc47745ace3452eb8
sliu107 <dldy0825@gmail.com>
2020-03-08 13:14:04 +0800

    add help command test class

---
+++ src/test/java/seedu/tp/commands/HelpCommandTest.java
@@ -0,0 +1,7 @@
+package seedu.tp.commands;
+
+public class HelpCommandTest {
+     /*
+    Execution of HelpCommand only involves UI interaction with users and is tested in text-ui-test.
+     */
+}
