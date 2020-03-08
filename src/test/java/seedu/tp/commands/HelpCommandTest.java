0c0fb086ed1d897acfc00242116fb390536ac6b7
sliu107 <dldy0825@gmail.com>
2020-03-08 13:50:15 +0800

    add javadoc t o HelpCommandTest class

--- src/test/java/seedu/tp/commands/HelpCommandTest.java
+++ src/test/java/seedu/tp/commands/HelpCommandTest.java
@@ -1,5 +1,8 @@
 package seedu.tp.commands;
 
+/**
+ * Execution test for help command.
+ */
 public class HelpCommandTest {
      /*
     Execution of HelpCommand only involves UI interaction with users and is tested in text-ui-test.
