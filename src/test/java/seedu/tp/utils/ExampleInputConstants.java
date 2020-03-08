78eeeae58c6ad36c4cdb1459fae8c41c10b72328
quinnyyy <quinnyyy@bu.edu>
2020-03-08 20:11:01 +0800

    Add tests for parseDate

--- src/test/java/seedu/tp/utils/ExampleInputConstants.java
+++ src/test/java/seedu/tp/utils/ExampleInputConstants.java
@@ -10,8 +10,8 @@ public class ExampleInputConstants {
     public static final String NAME = "Example name";
     public static final String START_DATE = "31 07 1843";
     public static final String END_DATE = "25 12 1892";
-    public static final LocalDate START_LOCAL_DATE = LocalDate.parse("1843-07-31");
-    public static final LocalDate END_LOCAL_DATE = LocalDate.parse("1892-12-25");
+    public static final LocalDate START_LOCAL_DATE = LocalDate.of(1843, 7, 31);
+    public static final LocalDate END_LOCAL_DATE = LocalDate.of(1892, 12, 25);
     public static final String SUMMARY = "Example summary";
     public static final String DETAIL_1 = "Detail 1";
     public static final String DETAIL_2 = "Detail 2";
