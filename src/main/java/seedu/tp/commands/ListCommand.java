b5d50c7338c0a4e61b908257e82f0d88e793913b
Zhilin <huangzhilin1999@gmail.com>
2020-03-13 01:00:59 +0800

    Add logging for ListCommand

--- src/main/java/seedu/tp/commands/ListCommand.java
+++ src/main/java/seedu/tp/commands/ListCommand.java
@@ -3,17 +3,39 @@ package seedu.tp.commands;
 import seedu.tp.flashcard.FlashcardList;
 import seedu.tp.ui.Ui;
 
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 /**
  * Command to list all existing flashcards.
  */
 public class ListCommand extends Command {
 
+    private static Logger logger = Logger.getLogger(ListCommand.class.getName());
     private FlashcardList flashcardList;
     private Ui ui;
 
+    /**
+     * Constructor for list command.
+     *
+     * @param flashcardList flashcard list for the command to execute on
+     * @param ui            the UI class to be used by the command
+     */
     public ListCommand(FlashcardList flashcardList, Ui ui) {
+        setupLogger();
         this.flashcardList = flashcardList;
         this.ui = ui;
+    }
+
+    private static void setupLogger() {
+        // Solution below referenced and adopted from: https://www.youtube.com/watch?v=W0_Man88Z3Q&feature=youtu.be
+        LogManager.getLogManager().reset();
+        logger.setLevel(Level.ALL);
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        consoleHandler.setLevel(Level.SEVERE);
+        logger.addHandler(consoleHandler);
     }
 
     /**
@@ -27,7 +49,9 @@ public class ListCommand extends Command {
 
     @Override
     public void execute() {
+        logger.info("Listing flashcards...");
         ui.listAllFlashcards(flashcardList);
+        logger.info("Listed flashcards");
     }
 
     @Override
