1a6ac54d9cb8daf4dead341083c858163cbcfaa2
g-lilian <guolilian98@gmail.com>
2020-03-14 14:37:49 +0800

    Remove constants from Priority and ReviewedCommandTest
    Removed constants in order to fix assertion error caused by constants being changed across different test classes.

--- src/test/java/seedu/tp/commands/PriorityCommandTest.java
+++ src/test/java/seedu/tp/commands/PriorityCommandTest.java
@@ -4,24 +4,48 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import seedu.tp.exceptions.InvalidFlashcardIndexException;
+import seedu.tp.flashcard.EventFlashcard;
 import seedu.tp.flashcard.Flashcard;
 import seedu.tp.flashcard.FlashcardList;
+import seedu.tp.flashcard.OtherFlashcard;
+import seedu.tp.flashcard.PersonFlashcard;
 import seedu.tp.ui.Ui;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static seedu.tp.utils.ExampleInputConstants.EVENT_FLASHCARD;
-import static seedu.tp.utils.ExampleInputConstants.PERSON_FLASHCARD;
-import static seedu.tp.utils.ExampleInputConstants.OTHER_FLASHCARD;
 import static seedu.tp.flashcard.Flashcard.PriorityLevel;
 
 public class PriorityCommandTest {
     private final ByteArrayOutputStream capturedOut = new ByteArrayOutputStream();
     private final PrintStream backupStdout = System.out;
+
+    public static final EventFlashcard EVENT_FLASHCARD = new EventFlashcard(
+            "Event 1",
+            LocalDate.of(1843, 7, 31),
+            LocalDate.of(1892, 12, 25),
+            "This is an event summary",
+            Arrays.asList("Detail 1", "Detail 2")
+    );
+
+    public static final PersonFlashcard PERSON_FLASHCARD = new PersonFlashcard(
+            "Person 1",
+            LocalDate.of(1843, 7, 31),
+            LocalDate.of(1892, 12, 25),
+            "This is a person's summary",
+            Arrays.asList("Detail 1", "Detail 2")
+    );
+
+    public static final OtherFlashcard OTHER_FLASHCARD = new OtherFlashcard(
+            "Title 1",
+            "This is a summary",
+            Arrays.asList("Detail 1", "Detail 2")
+    );
+
     private List<Flashcard> flashcards = Arrays.asList(EVENT_FLASHCARD, PERSON_FLASHCARD, OTHER_FLASHCARD);
     private FlashcardList flashcardList = new FlashcardList(flashcards);
 
