ff5def0e68b5e5cefe52598350a3ee1f56ffc078
g-lilian <guolilian98@gmail.com>
2020-03-15 18:36:35 +0800

    Add Javadoc comments to fix checkstyle
    Added Javadoc comments for GroupFactory and TimelineCommand.

--- src/main/java/seedu/tp/commands/TimelineCommand.java
+++ src/main/java/seedu/tp/commands/TimelineCommand.java
@@ -3,10 +3,19 @@ package seedu.tp.commands;
 import seedu.tp.flashcard.FlashcardList;
 import seedu.tp.ui.Ui;
 
+/**
+ * Command to show a timeline for the existing flashcards.
+ */
 public class TimelineCommand extends Command {
     private FlashcardList flashcardList;
     private Ui ui;
 
+    /**
+     * Constructor for TimelineCommand.
+     *
+     * @param flashcardList     list containing all flashcards
+     * @param ui                instance for user interaction
+     */
     public TimelineCommand(FlashcardList flashcardList, Ui ui) {
         assert flashcardList != null : "Invalid null FlashcardList!";
 
