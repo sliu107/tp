6e7ecde1ca07e5f88e7a99ad6cad0dce31b24c13
Zhilin <huangzhilin1999@gmail.com>
2020-03-19 23:52:57 +0800

    Add Junit tests for DisplayStudyPlanCommandTest and UpdateStudyPlanCommandTest

---
+++ src/test/java/seedu/tp/commands/UpdateStudyPlanCommandTest.java
@@ -0,0 +1,109 @@
+package seedu.tp.commands;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import seedu.tp.exceptions.HistoryFlashcardException;
+import seedu.tp.flashcard.EventFlashcard;
+import seedu.tp.flashcard.Flashcard;
+import seedu.tp.flashcard.FlashcardList;
+import seedu.tp.flashcard.OtherFlashcard;
+import seedu.tp.flashcard.PersonFlashcard;
+import seedu.tp.studyplan.StudyPlan;
+import seedu.tp.ui.Ui;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.time.LocalDate;
+import java.util.AbstractMap;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.tp.utils.ExampleInputConstants.DETAILS;
+import static seedu.tp.utils.ExampleInputConstants.END_LOCAL_DATE;
+import static seedu.tp.utils.ExampleInputConstants.LOCAL_DATE_1;
+import static seedu.tp.utils.ExampleInputConstants.LOCAL_DATE_2;
+import static seedu.tp.utils.ExampleInputConstants.SIMULATED_UPDATE_STUDY_PLAN_INPUT_1;
+import static seedu.tp.utils.ExampleInputConstants.SIMULATED_UPDATE_STUDY_PLAN_INPUT_2;
+import static seedu.tp.utils.ExampleInputConstants.START_LOCAL_DATE;
+
+public class UpdateStudyPlanCommandTest {
+
+    private static final EventFlashcard EVENT_FLASHCARD = new EventFlashcard(
+        "Event 1",
+        START_LOCAL_DATE,
+        END_LOCAL_DATE,
+        "This is an event summary",
+        DETAILS
+    );
+    private static final PersonFlashcard PERSON_FLASHCARD = new PersonFlashcard(
+        "Person 1",
+        START_LOCAL_DATE,
+        END_LOCAL_DATE,
+        "This is a person's summary",
+        DETAILS
+    );
+    private static final OtherFlashcard OTHER_FLASHCARD = new OtherFlashcard(
+        "Title 1",
+        "This is a summary",
+        DETAILS
+    );
+
+    private static final Map.Entry<LocalDate, List<Integer>> STUDY_PLAN_ENTRY_1 =
+        new AbstractMap.SimpleEntry<>(LOCAL_DATE_1, Arrays.asList(0, 1));
+    private static final Map.Entry<LocalDate, List<Integer>> STUDY_PLAN_ENTRY_2 =
+        new AbstractMap.SimpleEntry<>(LOCAL_DATE_2, Arrays.asList(1));
+    private static final InputStream backupStdin = System.in;
+    private FlashcardList fullFlashcardList;
+    private StudyPlan studyPlan;
+
+    /**
+     * Set up variables before each test.
+     */
+    @BeforeEach
+    public void setup() {
+        List<Flashcard> flashcards = Arrays.asList(EVENT_FLASHCARD, PERSON_FLASHCARD, OTHER_FLASHCARD);
+        fullFlashcardList = new FlashcardList(flashcards);
+        studyPlan = new StudyPlan();
+    }
+
+    @AfterEach
+    public void restoreStdin() {
+        System.setIn(backupStdin);
+    }
+
+    @Test
+    public void updateStudyPlanCommand_executeFromEmpty_success() throws HistoryFlashcardException {
+        ByteArrayInputStream simulatedSystemIn =
+            new ByteArrayInputStream(SIMULATED_UPDATE_STUDY_PLAN_INPUT_1.getBytes());
+        System.setIn(simulatedSystemIn);
+        UpdateStudyPlanCommand updateStudyPlanCommand = new UpdateStudyPlanCommand(new Ui(), studyPlan,
+            fullFlashcardList);
+        updateStudyPlanCommand.execute();
+
+        List<Map.Entry<LocalDate, List<Integer>>> expectedStudyPlanList =
+            Arrays.asList(STUDY_PLAN_ENTRY_1);
+        StudyPlan expectedStudyPlan = new StudyPlan(expectedStudyPlanList);
+
+        assertEquals(expectedStudyPlan, studyPlan);
+    }
+
+    @Test
+    public void updateStudyPlanCommand_execute_success() throws HistoryFlashcardException {
+        ByteArrayInputStream simulatedSystemIn =
+            new ByteArrayInputStream(SIMULATED_UPDATE_STUDY_PLAN_INPUT_2.getBytes());
+        System.setIn(simulatedSystemIn);
+        studyPlan = new StudyPlan(Arrays.asList(STUDY_PLAN_ENTRY_1));
+        UpdateStudyPlanCommand updateStudyPlanCommand = new UpdateStudyPlanCommand(new Ui(), studyPlan,
+            fullFlashcardList);
+        updateStudyPlanCommand.execute();
+
+        List<Map.Entry<LocalDate, List<Integer>>> expectedStudyPlanList =
+            Arrays.asList(STUDY_PLAN_ENTRY_1, STUDY_PLAN_ENTRY_2);
+        StudyPlan expectedStudyPlan = new StudyPlan(expectedStudyPlanList);
+
+        assertEquals(expectedStudyPlan, studyPlan);
+    }
+}
