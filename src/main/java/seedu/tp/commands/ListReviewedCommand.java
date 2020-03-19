6eda72fab7fc99d91a5ed070ec02517f501e9383
Zhilin <huangzhilin1999@gmail.com>
2020-03-20 00:54:27 +0800

    Implement ListReviewedCommand class

---
+++ src/main/java/seedu/tp/commands/ListReviewedCommand.java
@@ -0,0 +1,47 @@
+package seedu.tp.commands;
+
+import seedu.tp.flashcard.Flashcard;
+import seedu.tp.flashcard.FlashcardList;
+import seedu.tp.ui.Ui;
+
+import java.util.List;
+import java.util.Map;
+
+public class ListReviewedCommand extends Command {
+
+    private Ui ui;
+    private FlashcardList flashcardList;
+
+    /**
+     * Constructor for ListReviewedCommand.
+     *
+     * @param flashcardList the flashcard list for the command to execute on
+     * @param ui            the UI class for handling interaction with the user
+     */
+    public ListReviewedCommand(FlashcardList flashcardList, Ui ui) {
+        this.ui = ui;
+        this.flashcardList = flashcardList;
+    }
+
+    @Override
+    public void execute() {
+        LOGGER.info("Executing ListReviewedCommand...");
+        List<Map.Entry<Integer, Flashcard>> reviewedFlashcards = flashcardList.getAllReviewedFlashcards();
+        ui.listAllFlashcardsWithId(reviewedFlashcards);
+        LOGGER.info("Finished executing ListReviewedCommand!");
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof ListReviewedCommand)) {
+            return false;
+        }
+        if (this == obj) {
+            return true;
+        }
+
+        ListReviewedCommand otherListReviewedCommand = (ListReviewedCommand) obj;
+        return this.ui.equals(otherListReviewedCommand.ui)
+            & this.flashcardList.equals(otherListReviewedCommand.flashcardList);
+    }
+}
