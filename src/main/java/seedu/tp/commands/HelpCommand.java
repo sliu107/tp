4501f6550e144f9884b88f5403dc2969da44f221
Zhilin <huangzhilin1999@gmail.com>
2020-04-04 19:34:17 +0800

    Update HelpCommand for DeleteStudyPlanCommand

--- src/main/java/seedu/tp/commands/HelpCommand.java
+++ src/main/java/seedu/tp/commands/HelpCommand.java
@@ -40,8 +40,9 @@ public class HelpCommand extends Command {
         + "10d. List all flashcards in a group: list-group [GROUPNAME/INDEX]" + NEWLINE
         + NEWLINE
         + "STUDY PLANS" + NEWLINE
-        + "11a. Create or update today's study plan: plan" + NEWLINE
-        + "11b. Show all study plans: show-plan" + NEWLINE
+        + "11a. Create or update study plan for a day: plan" + NEWLINE
+        + "11b. Delete a study plan: delete-plan" + NEWLINE
+        + "11c. Show all study plans: show-plan" + NEWLINE
         + NEWLINE
         + "RANDOMIZED REVIEW" + NEWLINE
         + "12. Shuffle and display a random flashcard: random" + NEWLINE
