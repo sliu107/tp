9be341fcee1e1333d9e390c0fb8f4e7508026069
quinnyyy <quinnyyy@bu.edu>
2020-04-10 15:36:12 -0400

    Swallow errors if there is a JSON parse exception when loading files. This makes it so the application does not throw an exception if saved files are corrupted. They are just unused instead

--- src/main/java/seedu/tp/storage/Storage.java
+++ src/main/java/seedu/tp/storage/Storage.java
@@ -2,6 +2,8 @@ package seedu.tp.storage;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSyntaxException;
 import seedu.tp.exceptions.DeletionFailedException;
 import seedu.tp.exceptions.DuplicateFlashcardNameException;
 import seedu.tp.flashcard.Flashcard;
@@ -143,6 +145,8 @@ public class Storage {
                     LOGGER.info("File: " + f.toString() + " was loaded from disk.");
                 } catch (FileNotFoundException e) {
                     LOGGER.warning("File: " + f.toString() + " was not found when loading from disk.");
+                } catch (JsonParseException e) {
+                    LOGGER.warning("File: " + f.toString() + " could not be loaded (bad formatting).");
                 }
             }
         }
@@ -155,6 +159,8 @@ public class Storage {
                     LOGGER.info("File: " + f.toString() + " was loaded from disk.");
                 } catch (FileNotFoundException e) {
                     LOGGER.warning("File: " + f.toString() + " was not found when loading from disk.");
+                } catch (JsonParseException e) {
+                    LOGGER.warning("File: " + f.toString() + " could not be loaded (bad formatting).");
                 }
             }
         }
@@ -195,6 +201,8 @@ public class Storage {
                     return flashcardList;
                 } catch (FileNotFoundException e) {
                     LOGGER.warning("File: " + file.toString() + " was not found when loading from disk.");
+                } catch (JsonParseException e) {
+                    LOGGER.warning("File: " + file.toString() + " could not be loaded (bad formatting).");
                 }
             }
         }
@@ -232,6 +240,8 @@ public class Storage {
                     return studyPlanList;
                 } catch (FileNotFoundException e) {
                     LOGGER.warning("File: " + file.toString() + " was not found when loading from disk.");
+                } catch (JsonParseException e) {
+                    LOGGER.warning("File: " + file.toString() + " could not be loaded (bad formatting).");
                 }
             }
         }
