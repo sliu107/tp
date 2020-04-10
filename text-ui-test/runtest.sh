e64a2fcc1854c1fd36f915b4d57e10f3e3d2bd4b
Zhilin <huangzhilin1999@gmail.com>
2020-04-10 23:19:03 +0800

    Update text Ui testing for loading from storage

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -8,6 +8,7 @@ cd ..
 
 cd text-ui-test
 
+rm -rf logs/
 rm -rf historyflashcards/
 
 java -jar -ea $(find ../build/libs/ -mindepth 1 -print -quit) < input.txt > ACTUAL.TXT
@@ -15,9 +16,20 @@ java -jar -ea $(find ../build/libs/ -mindepth 1 -print -quit) < input.txt > ACTU
 diff EXPECTED.TXT ACTUAL.TXT
 if [ $? -eq 0 ]
 then
-    echo "Test passed!"
+    echo "Test 1 passed!"
+else
+    echo "Test 1 failed!"
+    exit 1
+fi
+
+java -jar -ea $(find ../build/libs/ -mindepth 1 -print -quit) < input2.txt > ACTUAL2.TXT
+
+diff EXPECTED2.TXT ACTUAL2.TXT
+if [ $? -eq 0 ]
+then
+    echo "Test 2 passed!"
     exit 0
 else
-    echo "Test failed!"
+    echo "Test 2 failed!"
     exit 1
 fi
